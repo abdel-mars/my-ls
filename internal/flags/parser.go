df9f8d502a500c1be7613e23eafb97c3d8511013
eabderrahma <elmahmoudiabderrahmane@gmail.com>
2026-02-25 15:20:59 +0000

    switch between flags add R

--- internal/flags/parser.go
+++ internal/flags/parser.go
@@ -1,1 +1,30 @@
 package flags
+
+// import "fmt"
+import "strings"
+
+
+type Flags struct {
+    Recursive bool
+}
+
+func Parse(args []string) (Flags, []string) {
+	flags := Flags{}
+	paths := []string{}
+
+	for _, arg := range args {
+		if strings.HasPrefix(arg, "-") && len(arg) > 1 {
+
+			for _, flag := range arg[1:] {
+				switch flag {
+				case 'R':
+					flags.Recursive = true
+				}
+			}
+		} else {
+			paths = append(paths, arg)
+		}
+}
+return flags, paths
+}
+
