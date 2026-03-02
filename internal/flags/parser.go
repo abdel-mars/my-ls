7e50fbf822b98613b874abb72603819639e609c6
eabderrahma <elmahmoudiabderrahmane@gmail.com>
2026-03-02 16:09:08 +0000

    clean parsing

--- internal/flags/parser.go
+++ internal/flags/parser.go
@@ -6,11 +6,16 @@ import "strings"
 
 type Flags struct {
     Recursive bool
+	All bool
 }
 
 func Parse(args []string) (Flags, []string) {
 	flags := Flags{}
 	paths := []string{}
+
+	// if !flags.All {
+	// 	fmt.Println("All is not true")
+	// }
 
 	for _, arg := range args {
 		if strings.HasPrefix(arg, "-") && len(arg) > 1 {
@@ -19,6 +24,8 @@ func Parse(args []string) (Flags, []string) {
 				switch flag {
 				case 'R':
 					flags.Recursive = true
+				case 'a':
+					flags.All = true
 				}
 			}
 		} else {
