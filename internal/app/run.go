6d7a549e343ca691f62fd3b91490162b46bd90cf
eabderrahma <elmahmoudiabderrahmane@gmail.com>
2026-03-02 16:08:28 +0000

    fixing falgs racall

--- internal/app/run.go
+++ internal/app/run.go
@@ -2,14 +2,17 @@ package app
 
 import (
 	"fmt"
+	// "strings"
+	"strings"
+	flagpkg "my-ls/internal/flags"
 	// "log"
+	// "my-ls/internal/flags"
 	"os"
 	"path/filepath"
-	"my-ls/internal/flags"
 )
 
 
-func ProcessPath(path string) {
+func ProcessPath(path string, flags flagpkg.Flags) {
 
 	entries, err := os.ReadDir(path)
 	if err != nil {
@@ -18,18 +21,25 @@ func ProcessPath(path string) {
 	}
 	fmt.Println(path + ":" )
 	for _, entry := range entries {
+		if !flags.All && strings.HasPrefix(entry.Name(), ".") {
+			continue
+		}
 		fmt.Println(entry.Name())
 	}
 	for _, entry := range entries {
+		if !flags.All && strings.HasPrefix(entry.Name(), ".") {
+			continue
+		}
+
 		if entry.IsDir() {
-			ProcessPath(filepath.Join(path, entry.Name()))
+		ProcessPath(filepath.Join(path, entry.Name()), flags)
 		}
 	}
 	
 
 }
 
-func ListOneLevel(path string) {
+func ListOneLevel(path string, flags flagpkg.Flags) {
 	entries, err := os.ReadDir(path)
 	if err != nil {
 		fmt.Println("my-ls", err)
@@ -37,21 +47,26 @@ func ListOneLevel(path string) {
 	}
 
 	for _, entry := range entries {
+		if !flags.All && strings.HasPrefix(entry.Name(), ".") {
+			continue
+		}
+
 		fmt.Println(entry.Name())
 	}
 }
 
 func Run(args []string) {
-	parsedFlags, paths := flags.Parse(args)
+	parsedFlags, paths := flagpkg.Parse(args)
 
 	if len(paths) == 0 {
 		paths = []string{"."}
 	}
 	for _, path := range paths {
 		if parsedFlags.Recursive {
-			ProcessPath(path)
-		} else {	
-			ListOneLevel(path)
+			ProcessPath(path, parsedFlags)
+		} else {
+			// fmt.Println("marsWasHere")
+			ListOneLevel(path, parsedFlags)
 		}
 	}
 
